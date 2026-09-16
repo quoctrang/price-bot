3bd3a312d5cc4ed29b1a00050aeb07abbb154881
quoctrang <35266368+quoctrang@users.noreply.github.com>
2026-09-16 19:32:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,26 +15,26 @@ HEADERS = {
 # Danh sách đầy đủ 100+ sản phẩm, đã cập nhật link ảnh theo yêu cầu của bạn cho các món tương ứng
 RAW_PRODUCTS = [
     # Nước khoáng, tinh khiết & nước ngọt (Đã cập nhật ảnh theo yêu cầu)
-    {"code": "n_01", "name": "Nước tinh khiết Aquafina 500ml", "base_price": 5500, "img": "https://image-cdn.7-eleven.vn/resize?type=webp&width=900&height=900&url="},
-    {"code": "n_03", "name": "Nước khoáng thiên nhiên La Vie 500ml", "base_price": 6500, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https://..."},
+    {"code": "n_01", "name": "Nước tinh khiết Aquafina 500ml", "base_price": 5500, "img": "https://image-cdn.7-eleven.vn/resize?type=webp&width=900&height=900&url=https%3A%2F%2Fhcm04.vstorage.vngcloud.vn%2Fssv-product-image%2F2722_7NOW-GRABMART%3APEPSI-SHOPEEFOOD%3APEPSI-7NOW%3APEPSI_1736759730404.jpg%3F1736759730434"},
+    {"code": "n_03", "name": "Nước khoáng thiên nhiên La Vie 500ml", "base_price": 6500, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https://cdnv2.tgdd.vn/bhx-static/bhx/Products/Images/2563/84812/bhx/slide-2_202410161047559036.jpg"},
     {"code": "n_04", "name": "Nước khoáng Dasani 500ml", "base_price": 5000, "img": "https://truongphatdat.com/wp-content/uploads/2019/12/Dasani-500ml.jpg"},
     {"code": "n_05", "name": "Nước khoáng chanh Ion-Life 330ml", "base_price": 7000, "img": "https://anbinhphat.com/wp-content/uploads/2018/08/chai-i-on-life-330ml.png"},
     {"code": "n_06", "name": "Nước uống vị trái cây 365 450ml", "base_price": 10000, "img": "https://down-vn.img.susercontent.com/file/vn-11134207-81ztc-mpbpkc2usa2z4b"},
-    {"code": "n_07", "name": "Nước ngọt có gas Pepsi lon 320ml", "base_price": 10000, "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSId8lxG8NQV1cL95fN..."},
-    {"code": "n_09", "name": "Nước ngọt Coca Cola lon 320ml", "base_price": 10500, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https://..."},
+    {"code": "n_07", "name": "Nước ngọt có gas Pepsi lon 320ml", "base_price": 10000, "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSId8lxZg8NQV1cL95fNghQNynjLt3c9BAuIJGqMPl_uA&s"},
+    {"code": "n_09", "name": "Nước ngọt Coca Cola lon 320ml", "base_price": 10500, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https:/cdn.tgdd.vn/Products/Images/2443/76451/bhx/nuoc-ngot-coca-cola-lon-320ml-202304131107525481.jpg"},
     
     # Nước tăng lực & bù khoáng (Đã cập nhật ảnh theo yêu cầu)
-    {"code": "tl_01", "name": "Nước tăng lực Redbull Thái Lan lon 250ml", "base_price": 12000, "img": "https://product.hstatic.net/1000288770/product/nuoc_tang_luc_redbull_thai_lon..."},
-    {"code": "tl_03", "name": "Nước tăng lực Sting hương dâu đỏ chai 330ml", "base_price": 10000, "img": "https://s3-hcmc02.higiocloud.vn/images/2023/09/sting-dau-330-20230915020452..."},
-    {"code": "tl_07", "name": "Nước tăng lực Monster Energy lon 355ml", "base_price": 35000, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https://..."},
-    {"code": "tl_08", "name": "Nước tăng lực Wake-up 247 chai 330ml", "base_price": 9500, "img": "https://s3-hcmc02.higiocloud.vn/images/2025/04/10625441-1--20250425201810.p..."},
-    {"code": "tl_09", "name": "Nước tăng lực Compact lon 250ml", "base_price": 11000, "img": "https://product.hstatic.net/1000288770/product/n_c_t_ng_l_c_compact_lon_245..."},
-    {"code": "tl_10", "name": "Nước bù khoáng Pocari Sweat 500ml", "base_price": 19000, "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRu0Rjo07Pcyyi3OOJc..."},
+    {"code": "tl_01", "name": "Nước tăng lực Redbull Thái Lan lon 250ml", "base_price": 12000, "img": "https://product.hstatic.net/1000288770/product/nuoc_tang_luc_redbull_thai_lon_250_ml_9a7118ec9977428d815db5c003e4e335_master.jpg"},
+    {"code": "tl_03", "name": "Nước tăng lực Sting hương dâu đỏ chai 330ml", "base_price": 10000, "img": "https://s3-hcmc02.higiocloud.vn/images/2023/09/sting-dau-330-20230915020452.png"},
+    {"code": "tl_07", "name": "Nước tăng lực Monster Energy lon 355ml", "base_price": 35000, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https://cdn.tgdd.vn/Products/Images/3226/142215/bhx/nuoc-tang-luc-monster-energy-lon-355ml-4-700x467.jpg"},
+    {"code": "tl_08", "name": "Nước tăng lực Wake-up 247 chai 330ml", "base_price": 9500, "img": "https://s3-hcmc02.higiocloud.vn/images/2025/04/10625441-1--20250425201810.png"},
+    {"code": "tl_09", "name": "Nước tăng lực Compact lon 250ml", "base_price": 11000, "img": "https://product.hstatic.net/1000288770/product/n__c_t_ng_l_c_compact_lon_245_ml_1_master.jpg"},
+    {"code": "tl_10", "name": "Nước bù khoáng Pocari Sweat 500ml", "base_price": 19000, "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRu0Rjo07Pcyyi3OOJqMviWnMivVrymHA3OWnR-l2r6XPwB9Q_7DnyySE&s=10"},
 
     # Mì gói & thực phẩm khô (Đã cập nhật ảnh theo yêu cầu)
     {"code": "m_01", "name": "Thùng 30 gói mì Hảo Hảo chua cay Acecook", "base_price": 125000, "img": "https://vn-test-11.slatic.net/p/317511aca84a001d9728abeaf3ba15ab.jpg"},
-    {"code": "m_02", "name": "Mì Omachi sườn hầm ngũ quả gói 80g", "base_price": 8500, "img": "https://img.websosanh.vn/v2/users/root_product/images/thung-30-goi-mi-khoa..."},
-    {"code": "m_03", "name": "Thùng 30 gói mì Omachi khoai tây", "base_price": 240000, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https://..."},
+    {"code": "m_02", "name": "Mì Omachi sườn hầm ngũ quả gói 80g", "base_price": 8500, "img": "https://img.websosanh.vn/v2/users/root_product/images/thung-30-goi-mi-khoai-tay-omac/xb1gxlvsl5qjv.jpg"},
+    {"code": "m_03", "name": "Thùng 30 gói mì Omachi khoai tây", "base_price": 240000, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https://cdn.tgdd.vn/Products/Images/2565/175895/bhx/thung-30-goi-mi-khoai-tay-omachi-xot-bo-ham-80g-202303141450332772.jpg"},
 
     # Các sản phẩm còn lại giữ nguyên danh mục đầy đủ ban đầu
     {"code": "n_02", "name": "Nước tinh khiết Dasani 1.5L", "base_price": 9500, "img": "https://img.tgdd.vn/imgt/bhx/f_webp,fit_outside,quality_95,s_720x584/https://cdn.tgdd.vn/Products/Images/2683/229643/bhx/nuoc-tinh-khiet-dasani-1-5l-202203071607519131.jpg"},
